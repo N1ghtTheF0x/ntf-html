7fc9206915547584f3691175354c61820d4de95f
N1ghtTheF0x <kilo.night.prower@gmail.com>
2025-09-24 22:45:40 +0200

    implemented basic render, may cause recursion though

--- source/render.ts
+++ source/render.ts
@@ -1,31 +1,66 @@
 import { HTMLElement } from "./element"
-import { HTMLRootElement } from "./root"
+import { HTMLRootElement } from "./containers/root"
 
 const __start_tag__ = <S extends string>(content: S) => `<${content}>` as const
 const __single_tag__ = <S extends string>(content: S) => `<${content} />` as const
 const __end_tag__ = <S extends string>(content: S) => `</${content}>` as const
 
-const SINGLE_TAGS = [
+const SINGLE_TAGS: Array<string> = [
     "img",
     "meta",
     "link",
     "input"
 ]
 
-export function renderChildren(elms: Array<any>)
+export function renderChildren(elms: Array<any>): string
 {
-    return ""
+    return elms.map(elm =>
+    {
+        if(HTMLElement.is(elm))
+            return renderElement(elm)
+        return String(elm)
+    }).join("")
 }
 
-export function renderElement(elm: HTMLElement)
-{
-    const START_TAG = __start_tag__(elm.renderStartTagContent())
-    const END_TAG = __end_tag__(elm.type)
 
-    return START_TAG + renderChildren(elm.children) + END_TAG
+export function renderProperties(elm: HTMLElement): string
+{
+    function __render__(key: string,value: any): string
+    {
+        if(typeof value == "boolean" && value)
+            return key
+        return `${key}="${String(value).replaceAll('"',"&quot;")}"`
+    }
+    const properties: HTMLElement.Properties = {}
+    for(const [key,value] of Object.entries(elm.properties))
+    {
+        if(typeof value == "undefined" || value === null) continue
+        if(typeof value == "string" && value.length === 0)
+            continue
+        properties[key] = value
+    }
+    if(typeof elm.id == "string")
+        properties["id"] = elm.id
+    if(elm.classList.size > 0)
+        properties["class"] = elm.className
+    return Object.entries(properties)
+    .map(([key,value]) => __render__(key,value))
+    .join(" ")
 }
 
-export function render(root: HTMLRootElement)
+export function renderStartTagContent(elm: HTMLElement): string
+{
+    return `${elm.type} ${renderProperties(elm)}`.trim()
+}
+
+export function renderElement(elm: HTMLElement): string
+{
+    if(SINGLE_TAGS.includes(elm.type))
+        return __single_tag__(renderStartTagContent(elm))
+    return __start_tag__(renderStartTagContent(elm)) + renderChildren(elm.children) + __end_tag__(elm.type)
+}
+
+export function render(root: HTMLRootElement): string
 {
     return HTMLRootElement.HEADER + renderElement(root)
 }
