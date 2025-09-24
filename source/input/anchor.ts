21bf9dac7cd5aad5c14687c89e7b950ec2272a32
N1ghtTheF0x <kilo.night.prower@gmail.com>
2025-09-24 22:46:14 +0200

    explicit return types

--- source/input/anchor.ts
+++ source/input/anchor.ts
@@ -2,7 +2,7 @@ import { HTMLElement } from "../element"
 
 export class HTMLAnchorElement extends HTMLElement
 {
-    public get href(){return this.properties["href"] as string}
+    public get href(): string {return String(this.properties["href"])}
     public set href(v){this.properties["href"] = v}
     public constructor(href: string,...children: Array<any>)
     {
--- source/media/image.ts
+++ source/media/image.ts
@@ -2,24 +2,23 @@ import { HTMLElement } from "../element"
 
 export class HTMLImageElement extends HTMLElement
 {
-    public get src(){return this.properties["src"] as string}
+    public get src(): string {return String(this.properties["src"])}
     public set src(v){this.properties["src"] = v}
-    public get alt(){return this.properties["alt"] as string}
+    public get alt(): string {return String(this.properties["alt"])}
     public set alt(v){this.properties["alt"] = v}
-    public get width(){return this.properties["width"] as number}
+    public get width(): number {return Number(this.properties["width"])}
     public set width(v){this.properties["width"] = v}
-    public get height(){return this.properties["height"] as number}
+    public get height(): number {return Number(this.properties["height"])}
     public set height(v){this.properties["height"] = v}
     public constructor(src?: string,alt?: string)
     {
         super("img")
-        this._render_options.noEndTag = true
         if(typeof src == "string")
             this.src = src
         if(typeof alt == "string")
             this.alt = alt
     }
-    public setUniformSize(x: number)
+    public setUniformSize(x: number): this
     {
         this.width = this.height = x
         return this
