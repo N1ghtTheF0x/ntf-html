24b2aef5406505cc05fe4759e2336a7e9727f4c4
N1ghtTheF0x <kilo.night.prower@gmail.com>
2025-09-24 22:46:59 +0200

    exported new files

--- source/index.ts
+++ source/index.ts
@@ -1,3 +1,11 @@
+// containers
+export * from "./containers/list"
+export * from "./containers/root"
+export * from "./containers/table"
+// core/head
+export * from "./core/head/link"
+export * from "./core/head/meta"
+export * from "./core/head/script"
 // core
 export * from "./core/body"
 export * from "./core/head"
@@ -6,8 +14,7 @@ export * from "./input/anchor"
 // media
 export * from "./media/image"
 // index
+export * from "./attributes"
 export * from "./element"
-export * from "./list"
-export * from "./root"
-export * from "./table"
-export * from "./utils"
+export * from "./render"
+export * from "./utilities"
