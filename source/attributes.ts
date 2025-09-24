5958b72d4b0d08bd91ba370094b54fbff635a3f6
N1ghtTheF0x <kilo.night.prower@gmail.com>
2025-09-24 22:45:12 +0200

    common attribute types

---
+++ source/attributes.ts
@@ -0,0 +1,7 @@
+export type Referrer = "no-referrer" | "origin" | "no-referrer-when-downgrade" | "origin-when-cross-origin" | "same-origin" | "strict-origin" | "strict-origin-when-cross-origin" | "unsafe-URL"
+export type ReferrerPolicy = "no-referrer" | "no-referrer-when-downgrade" | "origin" | "origin-when-cross-origin" | "unsafe-url"
+export type CORS = "anonymouse" | "use-credentials" | ""
+export type FetchPriority = "high" | "low" | "auto"
+export type Blocking = "render"
+export type Sizes = `${number}${"x" | "X"}${number}`
+export type Relationship = "alternate" | "author" | "bookmark" | "canonical" | "compression-dictionary" | "dns-prefetch" | "external" | "expect" | "help" | "icon" | "license" | "manifest" | "me" | "modulepreload" | "next" | "nofollow" | "noopener" | "noreferrer" | "opener" | "pingback" | "preconnect" | "prefetch" | "preload" | "prev" | "privacy-policy" | "search" | "stylesheet" | "tag" | "terms-of-service"
